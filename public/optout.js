2476c45a7036c75a512e5cb55b4715df957d813e
Stefan Brauenr <github@stefanbrauner.de>
2024-04-25 00:17:51 +0200

    Optout Javascript added

---
+++ public/optout.js
@@ -0,0 +1,10 @@
+function initTrackingCheckBox(id) {
+  const checkBox = document.getElementById(id);
+  checkBox.checked = _etracker.isTrackingEnabled();
+  checkBox.onclick = function () {
+    const tld = this.getAttribute('data-tld');
+    this.checked ? _etracker.enableTracking(tld) : _etracker.disableTracking(tld);
+  };
+}
+
+initTrackingCheckBox('trackingAllowed');
