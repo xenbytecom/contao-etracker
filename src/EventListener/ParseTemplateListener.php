67a6278e7f559ec90613dee98f4a22d79e21fb66
Stefan Brauenr <github@stefanbrauner.de>
2024-04-30 23:03:22 +0200

    Using parseTemplate hook instead of customizeSearch for search result tracking

---
+++ src/EventListener/ParseTemplateListener.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xenbyte\ContaoEtracker\EventListener;
+
+use Contao\BackendTemplate;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\FrontendTemplate;
+
+#[AsHook('parseTemplate')]
+class ParseTemplateListener
+{
+    public function __invoke(FrontendTemplate|BackendTemplate $template): void
+    {
+        if ('mod_search' === $template->getName() && isset($template->count)) {
+            $template->etrackerEnable = GeneratePageListener::isTrackingEnabled();
+            $template->nonce = GeneratePageListener::getNonce();
+        }
+    }
+}
