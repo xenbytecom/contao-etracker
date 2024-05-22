99dd3e52ef317c648e89426a842d7f08d1200f5a
Stefan Brauenr <github@stefanbrauner.de>
2024-05-22 21:50:18 +0200

    support for 401, 403, 404 and 503 error pages

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -15,7 +15,7 @@ use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 // Palettes
 $GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'etrackerEnable';
-$GLOBALS['TL_DCA']['tl_page']['subpalettes']['etrackerEnable'] = 'etrackerAccountKey,etrackerDomain,etrackerTrackingDomain,etrackerDebug,etrackerOptimiser,etrackerAreaname,etrackerDoNotTrack,etrackerNoJquery,etrackerExcludeFEUser,etrackerExcludeBEUser,etrackerCDIFEUser';
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['etrackerEnable'] = 'etrackerAccountKey,etrackerDomain,etrackerTrackingDomain,etrackerDebug,etrackerOptimiser,etrackerAreaname,etrackerDoNotTrack,etrackerNoJquery,etrackerExcludeFEUser,etrackerExcludeBEUser,etrackerCDIFEUser;etrackerEvents';
 
 PaletteManipulator::create()
     ->addLegend('etracker_legend', 'meta_legend')
@@ -30,6 +30,16 @@ PaletteManipulator::create()
     ->addField(['etrackerAreaname'], 'etracker_legend', PaletteManipulator::POSITION_APPEND)
     ->addField(['etrackerAreas'], 'etracker_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('regular', 'tl_page')
+;
+
+PaletteManipulator::create()
+    ->addLegend('etracker_legend', 'meta_legend')
+    ->addField(['etrackerPagename'], 'etracker_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField(['etrackerAreas'], 'etracker_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('error_401', 'tl_page')
+    ->applyToPalette('error_403', 'tl_page')
+    ->applyToPalette('error_404', 'tl_page')
+    ->applyToPalette('error_503', 'tl_page')
 ;
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['etrackerEnable'] = [
@@ -184,4 +194,21 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['etrackerCDIFEUser'] = [
         'type' => 'boolean',
         'default' => false,
     ],
+];
+$GLOBALS['TL_DCA']['tl_page']['fields']['etrackerEvents'] = [
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'eval' => [
+        'tl_class' => 'w50 cbx',
+        'multiple' => true,
+    ],
+    'foreignKey' => 'tl_etracker_events.title',
+    'sql' => [
+        'type' => 'blob',
+        'notnull' => false,
+    ],
+    'relation' => [
+        'type' => 'hasMany',
+        'load' => 'lazy'
+    ]
 ];
