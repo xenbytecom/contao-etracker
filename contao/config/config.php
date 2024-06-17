0f24e6e77729750bf5fb9e4873bf8568fc935d67
Stefan Brauenr <github@stefanbrauner.de>
2024-06-18 00:09:28 +0200

    fixed code style

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,4 +1,18 @@
 <?php
+
+/*
+ * etracker integration for Contao CMS
+ *
+ * Copyright (c) 2024 Xenbyte, Stefan Brauner
+ *
+ * @author     Stefan Brauner <https://www.xenbyte.com>
+ * @link       https://github.com/xenbytecom/contao-etracker
+ * @license    LGPL-3.0-or-later
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 use Xenbyte\ContaoEtracker\Model\EtrackerEventsModel;
@@ -11,4 +25,4 @@ $GLOBALS['BE_MOD']['system']['etracker-Events'] = [
 ];
 
 // Models
-$GLOBALS['TL_MODELS']['tl_etracker_events']  = EtrackerEventsModel::class;
+$GLOBALS['TL_MODELS']['tl_etracker_events'] = EtrackerEventsModel::class;
