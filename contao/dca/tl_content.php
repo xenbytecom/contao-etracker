f5fd3addf1ccae90a3b42cc9152d6e267e703ab6
Stefan Brauenr <github@stefanbrauner.de>
2024-06-17 22:31:47 +0200

    fixed: optout template caused error in Contao 4.13

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -18,7 +18,7 @@ declare(strict_types=1);
 use Xenbyte\ContaoEtracker\Controller\ContentElement\EtrackerOptoutController;
 
 // Palette for etracker Optout Module
-$GLOBALS['TL_DCA']['tl_content']['palettes'][EtrackerOptoutController::TYPE] = '{type_legend},type,headline,etrackerOptOutType;{template_legend:hide},customTpl;{protected_legend:hide},protected;{invisible_legend:hide},invisible,start,stop;';
+$GLOBALS['TL_DCA']['tl_content']['palettes'][EtrackerOptoutController::TYPE] = '{type_legend},type,etrackerOptOutType;{template_legend:hide},customTpl;{protected_legend:hide},protected;{invisible_legend:hide},invisible,start,stop;';
 
 // Fields for Optout Module
 $GLOBALS['TL_DCA']['tl_content']['fields']['etrackerOptOutType'] = [
