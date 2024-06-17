9a092cee9c6dd334e5b754f7f9242cbab3e6bd42
Stefan Brauenr <github@stefanbrauner.de>
2024-06-17 23:34:31 +0200

    updated german text

--- contao/languages/de/tl_etracker_events.php
+++ contao/languages/de/tl_etracker_events.php
@@ -15,18 +15,18 @@
 
 declare(strict_types=1);
 
-// legends
 use Xenbyte\ContaoEtracker\Model\EtrackerEventsModel;
 
+// legends
 $GLOBALS['TL_LANG']['tl_etracker_events']['title_legend'] = 'etracker Ereignis-Konfiguration';
 
 // fields
 $GLOBALS['TL_LANG']['tl_etracker_events']['title'] = ['Titel', 'Bezeichner für die interne Zuordnung, nur in Contao'];
 $GLOBALS['TL_LANG']['tl_etracker_events']['selector'] = ['CSS-Selektor', 'z. B. a[href=^tel:]'];
 $GLOBALS['TL_LANG']['tl_etracker_events']['object'] = ['Event-Objekt', 'zu verwendener Wert für das Objekt'];
-$GLOBALS['TL_LANG']['tl_etracker_events']['category'] = ['Event-Kategorie'];
-$GLOBALS['TL_LANG']['tl_etracker_events']['action'] = ['Event-Aktion', 'nur für Klassifizierung in etracker'];
-$GLOBALS['TL_LANG']['tl_etracker_events']['type'] = ['Event-Typ', 'nur für Klassifizierung in etracker'];
+$GLOBALS['TL_LANG']['tl_etracker_events']['category'] = ['Event-Kategorie', 'Kategorie zur Klassifizierung in etracker'];
+$GLOBALS['TL_LANG']['tl_etracker_events']['action'] = ['Event-Aktion', 'Aktion für Klassifizierung in etracker'];
+$GLOBALS['TL_LANG']['tl_etracker_events']['type'] = ['Event-Typ', 'Typ für Klassifizierung in etracker'];
 $GLOBALS['TL_LANG']['tl_etracker_events']['event'] = ['Ereignis', 'Ereignis-Vorlage und benutzerdefiniertes Ereignis'];
 $GLOBALS['TL_LANG']['tl_etracker_events']['event']['options'] = [
     EtrackerEventsModel::EVT_MAIL => 'Klick auf E-Mail-Link (mailto:)',
