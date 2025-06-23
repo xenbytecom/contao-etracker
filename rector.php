097930f636a1e3a2723b64d8beefd4f1aa09ab35
Stefan Brauenr <github@stefanbrauner.de>
2025-06-23 22:51:45 +0200

    added rector and change tools to vendor-bin

---
+++ rector.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Config\RectorConfig;
+use Contao\Rector\Set\ContaoSetList;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets()
+    ->withSets([
+        LevelSetList::UP_TO_PHP_81,
+
+        // https://getrector.com/blog/5-common-mistakes-in-rector-config-and-how-to-avoid-them
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+
+        ContaoSetList::CONTAO_50,
+        ContaoSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        ContaoSetList::FQCN
+    ]);
